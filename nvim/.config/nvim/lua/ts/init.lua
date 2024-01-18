a84c6e20b7c4d3e6f6150d70c29db7b79bd553c5
Stefan Trost <stefan@techscene.de>
2024-01-18 21:59:12 +0100

    started migrating to using Lazy instead of Packer

--- nvim/.config/nvim/lua/ts/init.lua
+++ nvim/.config/nvim/lua/ts/init.lua
@@ -1,13 +1,4 @@
 require("ts.remap")
 require("ts.set")
-require("ts.packer")
+require("ts.load_lazy")
 
--- Populate Quickfixlist with all files in directory of current file
-vim.keymap.set("n", "<leader>qd", ":set errorformat=%f | cexpr glob(expand('%:p:h')..'/*')<CR>")
-
--- accept left/right in merge confict view
-vim.keymap.set("n", "<leader>t", ":diffget //2<CR>")
-vim.keymap.set("n", "<leader>n", ":diffget //3<CR>")
-
--- restart lsp
-  vim.keymap.set("n", "<leader>ls" ,":LspRestart<CR>")
---
+++ nvim/.config/nvim/lua/ts/lazy/plugins.lua
@@ -0,0 +1,50 @@
+return {
+  'joerdav/templ.vim',
+  {
+    "folke/trouble.nvim",
+    config = function()
+      require("trouble").setup {
+        icons = false,
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  },
+  'mbbill/undotree',
+  'ThePrimeagen/harpoon',
+  'nvim-treesitter/playground',
+  'nvim-treesitter/nvim-treesitter-context',
+  'folke/neodev.nvim',
+  'eslint/eslint',
+  {
+    'Equilibris/nx.nvim',
+    requires = {
+      'nvim-telescope/telescope.nvim',
+    },
+    config = function()
+      require("nx").setup {}
+    end
+  },
+  {
+    "princejoogie/dir-telescope.nvim",
+    -- telescope.nvim is a required dependency
+    requires = {"nvim-telescope/telescope.nvim"},
+    config = function()
+      require("dir-telescope").setup({
+        -- these are the default options set
+        hidden = true,
+        no_ignore = false,
+        show_preview = true,
+      })
+    end,
+  },
+  {
+    "windwp/nvim-autopairs",
+    config = function() require("nvim-autopairs").setup {} end
+  },
+  "windwp/nvim-ts-autotag",
+  'nvim-tree/nvim-web-devicons',
+  "sindrets/diffview.nvim",
+  'joeveiga/ng.nvim',
+}
