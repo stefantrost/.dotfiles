14cfd7868cdf27e76d0c6563b9c334e75502b04b
Stefan Trost <stefan@techscene.de>
2024-08-07 14:38:29 +0200

    forgotten changes

--- nvim/.config/nvim/lua/ts/plugins/trouble.lua
+++ nvim/.config/nvim/lua/ts/plugins/trouble.lua
@@ -1,16 +1,37 @@
 return {
-    "folke/trouble.nvim",
-    config = function()
-      require("trouble").setup {
-        icons = false,
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-      }
-    vim.keymap.set("n", "<leader>tt", function() require("trouble").toggle() end)
-    vim.keymap.set("n", "<leader>tw", function() require("trouble").toggle("workspace_diagnostics") end)
-    vim.keymap.set("n", "<leader>td", function() require("trouble").toggle("document_diagnostics") end)
-    vim.keymap.set("n", "<leader>tq", function() require("trouble").toggle("quickfix") end)
-    vim.keymap.set("n", "<leader>tl", function() require("trouble").toggle("loclist") end)
-    vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
-    end
-  }
+	"folke/trouble.nvim",
+	opts = {}, -- for default options, refer to the configuration section for custom setup.
+	cmd = "Trouble",
+	keys = {
+		{
+			"<leader>tt",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
+		},
+		{
+			"<leader>tT",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "Buffer Diagnostics (Trouble)",
+		},
+		{
+			"<leader>cs",
+			"<cmd>Trouble symbols toggle focus=false<cr>",
+			desc = "Symbols (Trouble)",
+		},
+		{
+			"<leader>cl",
+			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+			desc = "LSP Definitions / references / ... (Trouble)",
+		},
+		{
+			"<leader>tL",
+			"<cmd>Trouble loclist toggle<cr>",
+			desc = "Location List (Trouble)",
+		},
+		{
+			"<leader>tQ",
+			"<cmd>Trouble qflist toggle<cr>",
+			desc = "Quickfix List (Trouble)",
+		},
+	},
+}
