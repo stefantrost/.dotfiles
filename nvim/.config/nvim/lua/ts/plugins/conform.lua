eee2389b13d967b631035c4bfcca98533af850ca
Stefan Trost <stefan@techscene.de>
2024-12-14 21:28:22 +0100

    nvim config in separate file per plugin

--- nvim/.config/nvim/lua/ts/plugins/conform.lua
+++ nvim/.config/nvim/lua/ts/plugins/conform.lua
@@ -31,7 +31,7 @@ return { -- Autoformat
 			--
 			-- You can use a sub-list to tell conform to run *until* a formatter
 			-- is found.
-			javascript = { { "prettierd", "prettier" } },
+			javascript = { { "prettier" } },
 			typescript = { "prettier" },
 			html = { "prettier" },
 		},
--- nvim/.config/nvim/lua/ts/set.lua
+++ nvim/.config/nvim/lua/ts/set.lua
@@ -27,3 +27,9 @@ vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 
 vim.opt.updatetime = 50
+
+vim.filetype.add({
+	pattern = {
+		[".*%.blade%.php"] = "blade",
+	},
+})
