7c86686d735a8bc8c53cc49adf6802d7b869dfee
Stefan Trost <stefan@techscene.de>
2024-12-25 11:37:20 +0100

    bug: nvim - floatnotes expand home directory

--- nvim/.config/nvim/plugin/floatnotes.lua
+++ nvim/.config/nvim/plugin/floatnotes.lua
@@ -1,51 +1,51 @@
 local state = {
-	floating = {
-		buf = -1,
-		win = -1,
-	},
+  floating = {
+    buf = -1,
+    win = -1,
+  },
 }
 
 local function create_floating_window(opts)
-	opts = opts or {}
-	local width = opts.width or math.floor(vim.o.columns * 0.8)
-	local height = opts.height or math.floor(vim.o.lines * 0.8)
+  opts = opts or {}
+  local width = opts.width or math.floor(vim.o.columns * 0.8)
+  local height = opts.height or math.floor(vim.o.lines * 0.8)
 
-	local col = math.floor((vim.o.columns - width) / 2)
-	local row = math.floor((vim.o.lines - height) / 2)
+  local col = math.floor((vim.o.columns - width) / 2)
+  local row = math.floor((vim.o.lines - height) / 2)
 
-	local notes_file = "/Users/stefan/Projects/notes/index.norg"
-	local buf = nil
-	if vim.api.nvim_buf_is_valid(opts.buf) then
-		buf = opts.buf
-	else
-		buf = vim.fn.bufadd(notes_file)
-		vim.fn.bufload(buf)
-	end
+  local notes_file = vim.fn.expand "$HOME/Projects/notes/index.norg"
+  local buf = nil
+  if vim.api.nvim_buf_is_valid(opts.buf) then
+    buf = opts.buf
+  else
+    buf = vim.fn.bufadd(notes_file)
+    vim.fn.bufload(buf)
+  end
 
-	local win_config = {
-		relative = "editor",
-		width = width,
-		height = height,
-		col = col,
-		row = row,
-		style = "minimal",
-		border = "rounded",
-		title = "NOTES",
-		title_pos = "center",
-		footer = notes_file,
-	}
+  local win_config = {
+    relative = "editor",
+    width = width,
+    height = height,
+    col = col,
+    row = row,
+    style = "minimal",
+    border = "rounded",
+    title = "NOTES",
+    title_pos = "center",
+    footer = notes_file,
+  }
 
-	local win = vim.api.nvim_open_win(buf, true, win_config)
+  local win = vim.api.nvim_open_win(buf, true, win_config)
 
-	return { buf = buf, win = win }
+  return { buf = buf, win = win }
 end
 
 local function toggle_notes()
-	if not vim.api.nvim_win_is_valid(state.floating.win) then
-		state.floating = create_floating_window({ buf = state.floating.buf })
-	else
-		vim.api.nvim_win_hide(state.floating.win)
-	end
+  if not vim.api.nvim_win_is_valid(state.floating.win) then
+    state.floating = create_floating_window { buf = state.floating.buf }
+  else
+    vim.api.nvim_win_hide(state.floating.win)
+  end
 end
 
 vim.api.nvim_create_user_command("Floatnotes", toggle_notes, {})
